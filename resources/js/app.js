dd428dcd1b8397313c545af44be1f924ab37cad2
Altabaev <altabaev2013@gmail.com>
2020-01-20 17:14:03 +0300

    Функционал поиска: - перенес Vue компоненты в отдельный каталог

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component('search-admin', require('./components/SearchAdmin.vue').default);
-Vue.component('spinner', require('./components/Spinner.vue').default);
-Vue.component('search-box', require('./components/SearchBox.vue').default);
+Vue.component('search-admin', require('./components/search/SearchAdmin.vue').default);
+Vue.component('spinner', require('./components/search/Spinner.vue').default);
+Vue.component('search-box', require('./components/search/SearchBox.vue').default);
 
 const app = new Vue({
     el: '#app',
