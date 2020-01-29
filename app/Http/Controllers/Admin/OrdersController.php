2902999b68da615dd98970f288f496e50ab937d5
Артем <argannov@yandex.ru>
2020-01-29 11:39:26 +0300

    Добавил деталку для заказа + распечатка заказа + создание документа в пдф формате

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Orders;
+use App\User;
+use App\Goods;
+use PDF;
 
 class OrdersController extends Controller
 {
@@ -16,5 +19,26 @@ class OrdersController extends Controller
             $count = $count+1;
         }
         return view('admin.order.allorders',['orders'=>$orders,'count'=>$count]);
+    }
+    public function oneOrder($slug) {
+        $order = Orders::where('id',$slug)->first();
+        $id = $order->id;
+        $user_id = $order->user_id;
+        $info_user = User::where('id',$user_id)->first();
+        $ProductInfo = $order->product_list;
+        $InfoProducts = Goods::whereIn('id',json_decode($ProductInfo,true))->get();
+        return view('admin.order.DetailOrder', ['order'=>$order,'id'=>$id,'user_info'=>$info_user,'products'=>$InfoProducts]);
+    }
+    public function printPDF($slug)
+    {
+        $order = Orders::where('id',$slug)->first();
+        $id = $order->id;
+        $user_id = $order->user_id;
+        $info_user = User::where('id',$user_id)->first();
+        $ProductInfo = $order->product_list;
+        $InfoProducts = Goods::whereIn('id',json_decode($ProductInfo,true))->get();
+
+        $pdf = PDF::loadView('admin.order.printPDF',['order'=>$order,'id'=>$id,'user_info'=>$info_user,'products'=>$InfoProducts]);
+        return $pdf->download('order'.$id.'.pdf');
     }
 }
